52493ec94762a25b7f8803361947ab28adcb9213
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-07-12 21:15:32 +0300

    feat: implement asynchronous email sending for /hello endpoint

---
+++ src/main/java/com/hei/school/endpoint/rest/controller/mail/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.hei.school.endpoint.rest.controller.mail;
+
+import com.hei.school.endpoint.event.EventProducer;
+import com.hei.school.model.SendEmailRequested;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class HelloWorldController {
+  private final EventProducer<SendEmailRequested> eventProducer;
+
+  @GetMapping("/hello")
+  @SneakyThrows
+  public String helloWorld(@RequestParam String to) {
+    var event = SendEmailRequested.builder().to(to).build();
+    eventProducer.accept(List.of(event));
+    return "... world!";
+  }
+}
