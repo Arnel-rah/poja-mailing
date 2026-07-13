90724591503c303c97cf66746869d45bed0d58b9
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-07-13 09:21:12 +0300

    feat(event): add UploadedFileSubmitted event

---
+++ src/main/java/com/hei/school/endpoint/event/model/UploadedFileSubmitted.java
@@ -0,0 +1,31 @@
+package com.hei.school.endpoint.event.model;
+
+import java.time.Duration;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class UploadedFileSubmitted extends PojaEvent {
+  private String uploadedFileId;
+  private String bucketKey;
+  private String email;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(45);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
+}
